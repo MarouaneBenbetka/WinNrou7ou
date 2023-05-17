c5ac685e75fde5557756fea42cc9c0bb10130b43
Marouane <MarouaneBenbetka@gmail.com>
2023-05-18 00:09:27 +0100

    tailwind config color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,11 @@ module.exports = {
 	],
 	theme: {
 		extend: {
-			backgroundImage: {
-				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-				"gradient-conic":
-					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			colors: {
+				green: "#D5DD18",
+				blue: "#069ADF",
+				orange: "#F7AF00",
+				dark: "#1D1D1B",
 			},
 		},
 	},
