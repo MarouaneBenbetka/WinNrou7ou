a90080d790e015ccebbf25dc1cdb2adecd8b8a1d
Marouane <MarouaneBenbetka@gmail.com>
2023-05-19 10:52:51 +0100

    collapse search bar feature + animation

--- components/hero/Search.jsx
+++ components/hero/Search.jsx
@@ -5,6 +5,7 @@ import React, { useState } from "react";
 import Selector from "./Selector";
 import Dropdown from "./Dropdown";
 import { wilayas } from "@/data/data";
+import { MdOutlineKeyboardDoubleArrowUp } from "react-icons/md";
 
 const Search = ({ onSearch, onFilter, stickTop }, ref) => {
 	const [showFilters, setShowFilters] = useState(false);
@@ -17,6 +18,7 @@ const Search = ({ onSearch, onFilter, stickTop }, ref) => {
 		type: false,
 		wilaya: false,
 	});
+	const [collapsedBar, setCollapsedBar] = useState(false);
 
 	const changeTypeHandler = (type) => {
 		setFilterData((prev) => ({ ...prev, typeAnnonce: type }));
@@ -29,6 +31,11 @@ const Search = ({ onSearch, onFilter, stickTop }, ref) => {
 		}));
 		// console.log(wilaya);
 	};
+
+	const toggleCollapseHanlder = () => {
+		setCollapsedBar((prev) => !prev);
+	};
+
 	const openTypeList = (state) => {
 		state
 			? setOpenedLists({
@@ -54,91 +61,138 @@ const Search = ({ onSearch, onFilter, stickTop }, ref) => {
 	});
 
 	return (
+		//     /	top: 0;
+		// background-color: #d5dd18dd;
+		// width: 80vw;
+		// margin-top: 0;
+		// border-radius: 0;
+		// border-bottom-left-radius: 80px;
+		// border-bottom-right-radius: 80px;
 		<div
 			ref={ref}
-			className={`${
-				showFilters
-					? "mt-10  px-6 py-3 bg-dark backdrop-filter backdrop-blur-lg bg-opacity-20 rounded-3xl  "
-					: "mt-10 "
-			} ${stickTop ? "stickySearch" : ""}`}
+			className={` ${
+				showFilters && !stickTop
+					? " px-6 py-3 bg-dark backdrop-filter backdrop-blur-lg bg-opacity-20 rounded-3xl  "
+					: ""
+			} ${
+				stickTop
+					? `${
+							!collapsedBar && "stickyAnimation"
+					  } w-[100vw] md:w-[80vw] mt-0 rounded-b-[30px] md:rounded-b-[80px] bg-green fixed top-0 `
+					: "mt-10"
+			}`}
 		>
 			<div className="flex items-center justify-center">
-				<div className="w-[70vw]  md:w-[60vw] lg:w-[40vw] relative flex flex-col  justify-center mr-4 ">
-					<button
-						type="submit"
-						className="absolute right-3"
-						onClick={(e) => {
-							e.preventDefault();
-							onSearch(searchedText);
-						}}
-					>
-						<FiSearch size="30px" color="#069ADF" />
-					</button>
-					<div className="my-4 ">
-						<input
-							type="text"
-							name="search"
-							placeholder="Explorer l'algerie ... "
-							className="text-dark w-full pl-6 pr-12 py-3 bg-white2 placeholder-gray-500 rounded-xl border-white ring-2 ring-gray-200 focus:outline-blue focus:outline-none  "
-							onChange={(e) => setSearchedText(e.target.value)}
-							onKeyDown={(e) => {
-								if (e.key === "Enter") {
-									setShowFilters(false);
+				<div
+					className={`${
+						stickTop
+							? "w-[65vw]  lg:w-[50vw]"
+							: "w-[70vw] md:w-[60vw] lg:w-[40vw]"
+					}   md:w-[60vw] lg:w-[40vw] relative flex flex-col  justify-center mr-4 `}
+				>
+					{stickTop && (
+						<div
+							className={`floatAnimation ${
+								collapsedBar
+									? "fixed -top-1 left-0 md:left-[70px]"
+									: "absolute -left-8 md:-left-24 rounded-3xl"
+							} cursor-pointer rounded-b-xl  bg-green`}
+							onClick={toggleCollapseHanlder}
+						>
+							<MdOutlineKeyboardDoubleArrowUp
+								color="#fff"
+								size={38}
+								className={
+									(collapsedBar ? "rotate-180" : "rotate-0") +
+									" transition-all ease-in-out duration-500"
 								}
-							}}
+							/>
+						</div>
+					)}
+					{(!stickTop || !collapsedBar) && (
+						<>
+							<button
+								type="submit"
+								className="absolute right-3"
+								onClick={(e) => {
+									e.preventDefault();
+									onSearch(searchedText);
+								}}
+							>
+								<FiSearch size="30px" color="#069ADF" />
+							</button>
+							<div className="my-4 pl-3 md:pl-0">
+								<input
+									type="text"
+									name="search"
+									placeholder="Explorer l'algerie ... "
+									className="text-dark w-full pl-6 pr-12 py-3 bg-white2 placeholder-gray-500 rounded-xl border-white ring-2 ring-gray-200 focus:outline-blue focus:outline-none  "
+									onChange={(e) =>
+										setSearchedText(e.target.value)
+									}
+									onKeyDown={(e) => {
+										if (e.key === "Enter") {
+											setShowFilters(false);
+										}
+									}}
+								/>
+							</div>
+						</>
+					)}
+				</div>
+				{(!stickTop || !collapsedBar) && (
+					<div
+						className="cursor-pointer"
+						onClick={() => setShowFilters(!showFilters)}
+					>
+						<VscSettings
+							size="34px"
+							className="rotate-90 hover:scale-[1.06]"
+							color="#069ADF"
 						/>
 					</div>
-				</div>
-				<div
-					className="cursor-pointer"
-					onClick={() => setShowFilters(!showFilters)}
-				>
-					<VscSettings
-						size="34px"
-						className="rotate-90 hover:scale-[1.06]"
-						color="#069ADF"
-					/>
-				</div>
+				)}
 			</div>
 
 			{/* filter bar */}
-
-			<animated.div
-				className={
-					"relative  z-10 " + (!showFilters && "overflow-hidden")
-				}
-				style={openAnimation}
-			>
-				<form
-					onSubmit={(e) => {
-						e.preventDefault();
-						onFilter(filterData);
-						setOpenedLists("");
-					}}
+			{(!stickTop || !collapsedBar) && (
+				<animated.div
+					className={
+						"relative  z-10 " + (!showFilters && "overflow-hidden")
+					}
+					style={openAnimation}
 				>
-					<div className=" mt-2 mb-4 flex gap-x-2 gap-y-[6px] justify-center items-end flex-wrap relative z-10 overflow-visible">
-						<Selector
-							label="Type"
-							onChangeValue={changeTypeHandler}
-							open={openedList.type}
-							onOpened={openTypeList}
-						/>
+					<form
+						onSubmit={(e) => {
+							e.preventDefault();
+							onFilter(filterData);
+							setOpenedLists("");
+						}}
+					>
+						<div className=" mt-2 mb-4 flex gap-x-2 gap-y-[6px] justify-center items-end flex-wrap relative z-10 overflow-visible">
+							<Selector
+								label="Type"
+								onChangeValue={changeTypeHandler}
+								open={openedList.type}
+								onOpened={openTypeList}
+							/>
 
-						<Dropdown
-							items={wilayas}
-							label="Wilaya"
-							onChangeValue={changeWilayaHandler}
-							open={openedList.wilaya}
-							onOpened={openWilayaList}
-							value={filterData.wilaya}
-						/>
+							<Dropdown
+								items={wilayas}
+								label="Wilaya"
+								onChangeValue={changeWilayaHandler}
+								open={openedList.wilaya}
+								onOpened={openWilayaList}
+								value={filterData.wilaya}
+							/>
 
-						<button className=" px-4 py-2 h-fit text-white bg-blue bg-opacity-70 rounded-[10px] font-semibold border-2 border-blue border-opacity-70  hover:bg-transparent hover:text-white  transition">
-							Filtrer
-						</button>
-					</div>
-				</form>
-			</animated.div>
+							<button className=" px-4 py-2 h-fit text-white bg-blue bg-opacity-70 rounded-[10px] font-semibold border-2 border-blue border-opacity-70  hover:bg-dark hover:text-white  transition">
+								Filtrer
+							</button>
+						</div>
+					</form>
+				</animated.div>
+			)}
 		</div>
 	);
 };
