e689c083404590db0ac5aeaf92650162fdbf4d92
Marouane <MarouaneBenbetka@gmail.com>
2024-01-30 18:09:32 +0100

    deployment fix

--- utils/services/url.js
+++ utils/services/url.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const env = process.env.NODE_ENV;
 
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://win-nrou7ou.vercel.app/";
 // env == "development"
 // 	? "http://localhost:3000"
-// 	: "https://win-nrou7ou.vercel.app/";
+// 	: "";
 
 export const instance = axios.create({
 	baseURL: BASE_URL,
